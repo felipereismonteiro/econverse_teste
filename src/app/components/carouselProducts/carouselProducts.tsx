e2602337af9f44e528c1ca1b0f48dfc42b663f89
felipe <felipe.dreis.monteiro@gmail.com>
2023-08-24 17:31:11 -0300

    feat: botoes funcionando

--- src/app/components/carouselProducts/carouselProducts.tsx
+++ src/app/components/carouselProducts/carouselProducts.tsx
@@ -8,8 +8,26 @@ import CardProduct from "./components/cardProduct/cardProduct";
 import "./carouselProducts.styles.scss";
 
 export default function CarouselProducts() {
-  const ScrollRef = useRef(null);
   const [products, setProducts] = useState<Products | undefined>();
+  const carouselRef = useRef<null | HTMLDivElement>(null);
+
+  const scrollLeft = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({
+        left: -400,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const scrollRight = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollBy({
+        left: 400,
+        behavior: "smooth",
+      });
+    }
+  };
 
   const formatNumber = (number: number) => {
     return numeral(number).format("0,0");
@@ -28,24 +46,26 @@ export default function CarouselProducts() {
   }, []);
 
   return (
-    <div className="productsContainer">
-      <div className="arrowLeftProducts">
-        <AiOutlineLeft/>
+    <div className="MainProductsContainer">
+      <div className="arrowLeftProducts" onClick={scrollLeft}>
+        <AiOutlineLeft />
       </div>
-      <div style={{ display: "flex" }} ref={ScrollRef}>
-        {products?.map((p, index) => (
-          <CardProduct
-            key={index}
-            image={p.photo}
-            description={p.descriptionShort}
-            price={formatNumber(p.price)}
-            priceBefore={formatNumber(p.price + 300)}
-            priceX={formatNumber(p.price / 2)}
-          />
-        ))}
+      <div className="productsContainer" ref={carouselRef}>
+        <div style={{ display: "flex" }}>
+          {products?.map((p, index) => (
+            <CardProduct
+              key={index}
+              image={p.photo}
+              description={p.descriptionShort}
+              price={formatNumber(p.price)}
+              priceBefore={formatNumber(p.price + 300)}
+              priceX={formatNumber(p.price / 2)}
+            />
+          ))}
+        </div>
       </div>
-      <div className="arrowRightProducts">
-          <AiOutlineRight/>
+      <div className="arrowRightProducts" onClick={scrollRight}>
+        <AiOutlineRight />
       </div>
     </div>
   );
