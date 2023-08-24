120b26755324fed18b2797a775046906834220cf
felipe <felipe.dreis.monteiro@gmail.com>
2023-08-23 22:34:29 -0300

    feat: footer completo

--- src/app/components/footer/components/upInfos/upInfos.tsx
+++ src/app/components/footer/components/upInfos/upInfos.tsx
@@ -1,6 +1,6 @@
+import "./style/upInfos.style.scss";
 import { AiOutlineInstagram, AiOutlineYoutube } from "react-icons/ai";
 import { FiFacebook } from "react-icons/fi";
-import "./style/upInfos.style.scss";
 import Image from "next/image";
 import VisaIcon from "@/../public/VISA.png";
 import elo from "@/../public/elo.png";
@@ -12,9 +12,13 @@ import pix from "@/../public/pix.png";
 import american from "@/../public/amex.png";
 import ticket from "@/../public/ticket.png";
 import sodexo from "@/../public/sodexo.png";
-
+import { FormEvent } from "react";
 
 export default function UpInfos() {
+  const handleSubmitEmail = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  }
+
   const aboutUs = ["CONHEÇA", "COMO FUNCIONA", "INDICAÇÃO E DESCONTO"];
 
   const usefulInfos = [
@@ -56,20 +60,30 @@ export default function UpInfos() {
       <div className="containerIconsPayment">
         <h1 className="titulo">FORMAS DE PAGAMENTO</h1>
         <div className="iconsPayment">
-          <Image src={VisaIcon} alt="paymentIcon"/>
-          <Image src={elo} alt="paymentIcon"/>
-          <Image src={alelo} alt="paymentIcon"/>
-          <Image src={dinners} alt="paymentIcon"/>
-          <Image src={ifood} alt="paymentIcon"/>
-          <Image src={mastercard} alt="paymentIcon"/>
-          <Image src={pix} alt="paymentIcon"/>
-          <Image src={american} alt="paymentIcon"/>
-          <Image src={ticket} alt="paymentIcon"/>
-          <Image src={sodexo} alt="paymentIcon"/>
+          <Image src={VisaIcon} alt="paymentIcon" />
+          <Image src={elo} alt="paymentIcon" />
+          <Image src={alelo} alt="paymentIcon" />
+          <Image src={dinners} alt="paymentIcon" />
+          <Image src={ifood} alt="paymentIcon" />
+          <Image src={mastercard} alt="paymentIcon" />
+          <Image src={pix} alt="paymentIcon" />
+          <Image src={american} alt="paymentIcon" />
+          <Image src={ticket} alt="paymentIcon" />
+          <Image src={sodexo} alt="paymentIcon" />
         </div>
       </div>
       <div></div>
-      <div className="novidades"></div>
+      <form onSubmit={handleSubmitEmail} className="novidades">
+        <div>
+          <h1 className="titleMainTitle">CADASTRE-SE E RECEBA NOSSAS</h1>
+          <h1 className="bolder">NOVIDADES E PROMOÇÕES</h1>
+        </div>
+        <p style={{ color: "#3F3F40", textAlign: "center", fontSize: 12 }}>Excepteur sint occaecat cudatat non ent, sunt in culpa qui officia lorem ipsum</p>
+        <div className="submitForm">
+          <input className="inputSubmit" type="text" placeholder="SEU EMAIL"/>
+          <button className="buttonSubmit" type="submit">OK</button>
+        </div>
+      </form>
     </div>
   );
 }
