6cfa908c8079c471120ff2d066325bb4525f71c7
felipereismonteiro <felipe.dreis.monteiro@gmail.com>
2023-08-24 12:32:39 -0300

    feat: separate text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import MainPageCategoryInside from "./components/mainPageCategoryInside/mainPageCategoryInside";
 import MainPageIconsCategory from "./components/mainPageIconsCategory/mainPageIconsCategory";
 import PromoBanner from "./components/promoBanner/promoBanner";
+import "./styles/homepage.styles.scss";
 
 export default function Home() {
-  return(
+  return (
     <div className="mainPageContainer">
       <PromoBanner />
-      <MainPageIconsCategory/>
-      <MainPageCategoryInside/>
+      <MainPageIconsCategory />
+      <div className="containerSeparate">
+        <div className="linewidth"></div>
+        <h1>Produtos relacionados</h1>
+        <div className="linewidth"></div>
+      </div>
+      <MainPageCategoryInside />
     </div>
-  )
+  );
 }
